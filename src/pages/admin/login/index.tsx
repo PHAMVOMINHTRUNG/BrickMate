d303c41a52964d6b1a0cba2f508b12397ee17dbc
taphuong <taphuong@cmcglobal.vn>
2022-10-24 14:41:10 +0700

    init project

---
+++ src/pages/admin/login/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface ILogonProps{
+    className?:string;
+}
+
+const LoginComponent:FC<ILogonProps> = (props) =>{
+    return <>
+        <h1>Loginnn</h1>
+    </>
+}
+
+export default LoginComponent;
